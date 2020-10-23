2fb9233e84420f1f1120f9f0155b77f1c940cc8d
Jonathan Duggan <jonathan@merxnet.io>
2020-10-23 00:33:22 -0400

    initial commit

---
+++ worker.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+import boto3
+import json
+import os
+from configparser import ConfigParser
+from sys import argv
+from sys import exit
+
+## parameters provided by Deluge
+script_dir = os.path.dirname(os.path.realpath(__file__))
+ini = script_dir + '/config.ini'
+
+if os.path.isfile(ini):
+  config = ConfigParser()
+  config.read(ini)
+  user = config.get('Main', 'User')
+  t_host = config.get('Main', 'Host')
+  target_dir = config.get('Main', 'Target')
+  queue_url = config.get('SQS', 'URL')
+
+  try:
+    profile = config.get('Credentials', 'Profile')
+  except:
+    print('No AWS credentials profile provided. Using default...')
+    profile = 'default'
+else:
+  queue_url = os.environ['SQS_URL']
+
+with open(script_dir + '/attributes.json', 'r') as f:
+    attrs = f.read()
+
+session = boto3.session.Session(profile_name=profile)
+sqs = session.client('sqs')
+
+message = sqs.receive_message(
+  QueueUrl=queue_url,
+  MessageAttributeNames=['All'],
+  MaxNumberOfMessages=1
+)
+
+try:
+  handles = [msg['ReceiptHandle'] for msg in message['Messages']]
+  attrs = [msg['MessageAttributes'] for msg in message['Messages']]
+except:
+  print('No messages in queue.')
+  exit(0)
+
+for handle in handles:
+  deleted = sqs.delete_message(
+    QueueUrl=queue_url,
+    ReceiptHandle=handle
+  )
+  print('Deleted message {}'.format(handle))
+
+p = 5
+s = 4
+
+for attr in attrs:
+  t_path = attr['Path']['StringValue']
+  t_name = attr['Name']['StringValue']
+  cmd = "bbcp -Ar -P {} -s {} {}@{}:{}/'{}' {}/'{}'".format(p, s, user, t_host, t_path, t_name, target_dir, t_name)
+  print(cmd)
+  res = os.system(cmd)
